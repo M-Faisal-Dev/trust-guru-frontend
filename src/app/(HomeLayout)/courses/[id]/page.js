e7f3e9a9096cbea0122c6f8a9fa2f9453eba0213
unknown <faisalgojra339@gmail.com>
2024-06-18 13:02:49 +0500

    use a strong error handling

--- src/app/(HomeLayout)/courses/[id]/page.js
+++ src/app/(HomeLayout)/courses/[id]/page.js
@@ -1,23 +1,27 @@
-
-
-import CourseHero from '@/components/Coaching-detail/CourseHero'
-import Graphics from '@/components/Coaching-detail/Graphics'
+import CourseHero from '@/components/Coaching-detail/CourseHero';
+import Graphics from '@/components/Coaching-detail/Graphics';
 
 async function getData(id) {
-  try{
-   const res = await fetch(`http://localhost:4000/api/course/${id}`, { cache: 'no-store' })
-    return res.json()
-  }catch(err){
-    throw new Error('Failed to fetch data')
+  try {
+    const res = await fetch(`http://localhost:4000/api/course/${id}`, { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (err) {
+    console.error(err); // Log the error for debugging purposes
+    return []; // Return an empty array or default value in case of error
   }
-  
 }
- 
-export default async function page({params}) {
-  const {id} = params
-  const data = await getData(id)
-  return <main> 
-     <CourseHero data={data ? data : []}/>
-     <Graphics data={data ? data : []} />
-     </main>
+
+export default async function Page({ params }) {
+  const { id } = params;
+  const data = await getData(id);
+
+  return (
+    <main>
+      <CourseHero data={data} />
+      <Graphics data={data} />
+    </main>
+  );
 }
--- src/app/(HomeLayout)/courses/page.js
+++ src/app/(HomeLayout)/courses/page.js
@@ -1,16 +1,24 @@
-import Course from "@/components/Course-detail/Course"
+import Course from "@/components/Course-detail/Course";
 
 async function getData() {
-  try{
-   const res = await fetch('http://localhost:4000/api/course', { cache: 'no-store' })
-    return res.json()
-  }catch(err){
-    throw new Error('Failed to fetch data')
+  try {
+    const res = await fetch('http://localhost:4000/api/course', { cache: 'no-store' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return res.json();
+  } catch (err) {
+    console.error(err); // Log the error for debugging purposes
+    return []; // Return an empty array or default value in case of error
   }
-  
 }
- 
+
 export default async function Page() {
-  const data = await getData()
-  return <main>  <Course data={data ? data : []}/></main>
+  const data = await getData();
+
+  return (
+    <main>
+      <Course data={data} />
+    </main>
+  );
 }
